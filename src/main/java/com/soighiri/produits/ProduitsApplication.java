1f7dd0b81ee31809039bd7279bc3d2e9b0daf4e9
SaidSoighiri94 <msaidsoighir@gmail.com>
2023-11-29 01:42:50 +0100

    //Implementation de la methode permettant d'afficher les Ids des produit avec l'utilisation @RepositoryRestResource

--- src/main/java/com/soighiri/produits/ProduitsApplication.java
+++ src/main/java/com/soighiri/produits/ProduitsApplication.java
@@ -1,13 +1,26 @@
 package com.soighiri.produits;
 
+import com.soighiri.produits.entities.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
-public class ProduitsApplication {
+public class ProduitsApplication implements CommandLineRunner {
+	@Autowired
 
-	public static void main(String[] args) {
+	private RepositoryRestConfiguration repositoryRestConfiguration;
+
+	public static void main(String[] args){
 		SpringApplication.run(ProduitsApplication.class, args);
 	}
 
+	//Implementation de la mmethode permettant d'afficher les Ids des produit avec l'utilisation @RepositoryRestResource
+	@Override
+	public void run(String... args) throws Exception {
+	repositoryRestConfiguration.exposeIdsFor(Produit.class);
+	}
 }
