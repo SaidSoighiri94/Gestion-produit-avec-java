8070c53ba9308cfcad46c2dbf228559a9ec70c42
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-05-02 13:42:23 +0200

    Utilisation de springDataRest dans la classe Categorie

--- src/main/java/com/soighiri/produits/ProduitsApplication.java
+++ src/main/java/com/soighiri/produits/ProduitsApplication.java
@@ -1,5 +1,6 @@
 package com.soighiri.produits;
 
+import com.soighiri.produits.entities.Categorie;
 import com.soighiri.produits.entities.Produit;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,7 @@ public class ProduitsApplication implements CommandLineRunner {
 	//Implementation de la methode permettant d'afficher les Ids des produit avec l'utilisation @RepositoryRestResource
 	@Override
 	public void run(String... args) throws Exception {
-	repositoryRestConfiguration.exposeIdsFor(Produit.class);
+	repositoryRestConfiguration.exposeIdsFor(Produit.class,Categorie.class);
 	}
 	@Bean
 	public ModelMapper modelMapper() {
--- src/main/java/com/soighiri/produits/repos/CategorieRepository.java
+++ src/main/java/com/soighiri/produits/repos/CategorieRepository.java
@@ -1,9 +1,14 @@
 package com.soighiri.produits.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.soighiri.produits.entities.Categorie;
 
+
+@RepositoryRestResource(path="cat")     // Utilisation du SpringDataRest 
+@CrossOrigin("htt://localhost:4200/")   //Pour autoriser angular a acceder cette resource 
 public interface CategorieRepository extends JpaRepository<Categorie, Long>{
 
 }
