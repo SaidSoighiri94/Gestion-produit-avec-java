de4874d154269cff5f6ba4a26f0b9430f2bab3e4
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-08-23 17:13:01 +0200

    Implementaion de la classe JWTAuthorization et l'interface SecParams

---
+++ src/main/java/com/soighiri/produits/security/SecParams.java
@@ -0,0 +1,9 @@
+package com.soighiri.produits.security;
+
+public interface SecParams {
+    public static final String PREFIX = "Bearer ";
+    public static final String SECRET = "Soighiri";
+    public static final long EXPIRE_TIME = 10*24*60*1000;
+
+
+}
